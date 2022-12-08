3924207753f900816cafa7e800add5899c097286
Fatman9000 <conman0503@gmail.com>
2022-12-08 15:02:20 -0500

    added flaskrun config

---
+++ flaskrun.py
@@ -0,0 +1,33 @@
+import optparse
+
+
+def flaskrun(app, default_host="0.0.0.0", default_port="8080"):
+
+    parser = optparse.OptionParser()
+    parser.add_option(
+        "-H",
+        "--host",
+        help="Hostname of the Flask app " + "[default %s]" % default_host,
+        default=default_host,
+    )
+    parser.add_option(
+        "-P",
+        "--port",
+        help="Port for the Flask app " + "[default %s]" % default_port,
+        default=default_port,
+    )
+
+    parser.add_option(
+        "-d", "--debug", action="store_true", dest="debug", help=optparse.SUPPRESS_HELP
+    )
+    parser.add_option(
+        "-p",
+        "--profile",
+        action="store_true",
+        dest="profile",
+        help=optparse.SUPPRESS_HELP,
+    )
+
+    options, _ = parser.parse_args()
+
+    app.run(debug=options.debug, host=options.host, port=int(options.port))
