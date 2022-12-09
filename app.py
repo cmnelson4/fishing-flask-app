94ba25a11ecedb673edb22a79fd3fb31cd45c535
Fatman9000 <conman0503@gmail.com>
2022-12-08 22:15:55 -0500

    made a much more interesting game

--- app.py
+++ app.py
@@ -1,43 +1,91 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flaskrun import flaskrun
+import random
 
 app = Flask(__name__)
 
-fish_caught = 0
-bait = "worm"
-rod = "basic"
+# define a variable to keep track of the player's money
+money = 0
 
+# define a variable to keep track of the player's fishing skill
+skill = 1
 
-@app.route("/")
+# define a list of fish with their corresponding values
+fish = [
+    {"name": "Goldfish", "value": 1},
+    {"name": "Salmon", "value": 5},
+    {"name": "Tuna", "value": 10},
+    {"name": "Shark", "value": 50},
+]
+
+# define a list of upgrades that the player can buy
+upgrades = [
+    {"name": "Better bait", "price": 5, "increase": 2},
+    {"name": "Faster boat", "price": 10, "increase": 5},
+    {"name": "Sonar", "price": 20, "increase": 10},
+]
+
+# define a route for the homepage
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html", fish_caught=fish_caught, bait=bait, rod=rod)
+    global money
+    global skill
+    return render_template(
+        "index.html", money=money, skill=skill, fish=fish, upgrades=upgrades
+    )
 
 
-@app.route("/upgrade_bait")
-def upgrade_bait():
-    global bait
-    if bait == "worm":
-        bait = "fly"
-    elif bait == "fly":
-        bait = "lure"
-    return render_template("index.html", fish_caught=fish_caught, bait=bait, rod=rod)
+# define a route for the fishing action
+@app.route("/fish", methods=["POST", "GET"])
+def go_fish():
+    global money
+    global skill
+
+    # determine which fish the player catches
+    caught_fish = fish[
+        random.randint(0, 3)
+    ]  # for now, always catch the first fish in the list
+
+    # increase the player's money by the value of the caught fish
+    money += caught_fish["value"] * skill
+
+    return render_template(
+        "index.html", money=money, skill=skill, fish=fish, upgrades=upgrades
+    )
 
 
-@app.route("/upgrade_rod")
-def upgrade_rod():
-    global rod
-    if rod == "basic":
-        rod = "medium"
-    elif rod == "medium":
-        rod = "advanced"
-    return render_template("index.html", fish_caught=fish_caught, bait=bait, rod=rod)
+# define a route for buying upgrades
+@app.route("/buy/<upgrade_name>", methods=["POST", "GET"])
+def buy(upgrade_name):
+    global money
+    global skill
+    global upgrades
+    # find the upgrade that the player is trying to buy
+    upgrade = None
+    for u in upgrades:
+        if u["name"] == upgrade_name:
+            upgrade = u
+            break
 
+    # make sure the upgrade exists
+    if upgrade:
+        # make sure the player has enough money to buy the upgrade
+        if money >= upgrade["price"]:
+            # decrease the player's money by the price of the upgrade
+            money -= upgrade["price"]
 
-@app.route("/cast")
-def cast():
-    global fish_caught
-    fish_caught += 1
-    return render_template("index.html", fish_caught=fish_caught, bait=bait, rod=rod)
+            # increase the player's fishing skill by the increase of the upgrade
+            skill += upgrade["increase"]
+
+    upgrades = [
+        {"name": "Better bait", "price": skill * 5, "increase": 2},
+        {"name": "Faster boat", "price": skill * 10, "increase": 5},
+        {"name": "Sonar", "price": skill * 20, "increase": 10},
+    ]
+
+    return render_template(
+        "index.html", money=money, skill=skill, fish=fish, upgrades=upgrades
+    )
 
 
 if __name__ == "__main__":
